2624a3d60823654795988d7ad3d7af18c19240ed
Emir <emirmuhammad08@yahoo.com>
2021-10-08 10:55:50 +0000

    Wrote Example 2 for variables etc

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -3,6 +3,22 @@ int main(){
     //this is a comment
     /* multiline comment
     */
-   std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
 
+    int a{4};
+    double b = 4.2;
+
+    a = 4 + 1;
+
+    int c = b; //no flags
+    int d{b}; //there is a warning
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+
+
+
+    return 0;
 }
