46d424d2d48a5f1dd8f3b642299fefee582e1c53
Emir <emirmuhammad08@yahoo.com>
2021-10-08 13:08:48 +0000

    Complete first cpp exercises in mpags-cipher

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <string>
 int main(){
     //this is a comment
     /* multiline comment
     */
-    std::cout << "Hello World!\n";
 
+    /* Exercise 1
+    std::cout << "Hello World!\n";
+    */
+
+    //Exercise 2
     int a{4};
     double b = 4.2;
 
@@ -18,7 +23,33 @@ int main(){
     std::cout << c << std::endl;
     std::cout << d << std::endl;
 
+    
 
+    /* //Exercise 3
+    const double e{9.81};
+    std::cout << e << std::endl;
+    int f{7}; //const will not compile
+    f = 8;
+
+    std::string mymsg = "Hello There";
+    std::cout << mymsg << std::endl;
+
+    */ 
+   
+    //Exercise 4
+    int g{9};
+    double h{10.1};
+
+    std::cout << h/g << std::endl;
+    std::cout << g/g << std::endl;
+
+    std::string newmsg = "General Kenobi";
+    char newchar = newmsg[2];
+    std::cout << newchar << std::endl;
+    
+
+
+    
 
     return 0;
 }
