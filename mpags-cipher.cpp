35ff6806aa8a017553221edcafc839777f4e66b2
Emir <emirmuhammad08@yahoo.com>
2021-10-11 10:55:45 +0000

    Fixed help text and command line argument handling

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -41,62 +41,76 @@ std::string changenumtochar(char c){
 }
 
 int main(int argc, char* argv[]){
+
+    //Section that handles command line arguments
     const std::vector<std::string> cmdLineArgs { argv, argv + argc};
 
+    bool helpflag = false;
+    std::string inputfile = "default.txt";
+    std::string outputfile = "default.txt";
+
     for(size_t i{0}; i < cmdLineArgs.size(); ++i){
-        //std::cout << cmdLineArgs[i] << std::endl;
         if(cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help"){
-            std::cout << "Usage: ./mpags-cipher" << std::endl;
-            std::cout << "Options:" << std::endl;
-            std::cout << "there are no options at the moment" << std::endl;
-            //return 0;
+            helpflag = true;
         }
+
         if(cmdLineArgs[i] == "--version"){
             std::cout << "Version: 0.1.0" << std::endl;
-
         }
         if(cmdLineArgs[i] == "-i"){
-            std::cout << "input: " << cmdLineArgs[i+1] << std::endl;
-            //do some storing stuff here, for now just return0
-            //return 0;
-            
+            inputfile = cmdLineArgs[i+1]; 
+            std::cout << "input: " << inputfile << std::endl;
         }
+
         if(cmdLineArgs[i] == "-o"){
-            std::cout << "output: " << cmdLineArgs[i+1] << std::endl;
-            //do some storing stuff here, for now just return0
-            //return 0;
-            
+            outputfile = cmdLineArgs[i+1]; 
+            std::cout << "output: " << outputfile << std::endl;
         }
-        
     }
 
-    /*
-    Take each letter from user input and in each case
-    - convert to upper case    
-    - change numbers to words
-    - Ignore any other (non-alpha characters)
-    - In each case, add result to a string variable
-    print out the new string
-    */
-   
-    //initialise a couple of important variables
-    char in_char{'y'};
+    if(helpflag == true){
+        std::cout << "Usage: ./mpags-cipher [options]" << std::endl;
+        std::cout << " " << std::endl;
+        std::cout << "Description:" << std::endl;
+        std::cout << "(for now) Takes in text from file or input at terminal    " << std::endl;
+        std::cout << "and transcribes it to ciphertext." << std::endl;
+        std::cout << "Removes non-alphanumerical text, converts numbers to text, and capitalises" << std::endl;
+        std::cout << "When inputting text from command line, press ctr+D to stop and transcribe" << std::endl;
+        std::cout << " " << std::endl;
+        std::cout << "Options:" << std::endl;
+        std::cout << "  -i  [filename] Specify the input file" << std::endl;
+        std::cout << "  -o  [filename] Specify the output file" << std::endl;
+        std::cout << "  --version      Print current version of program" << std::endl;
+        std::cout << "  -h, --help     Print the current help text" << std::endl;
+        return 0;
+        /*
+        Note: the help text code was written with the intent that anytime the help text is called
+        the program will not run at all, because the user wants to see the help text and not run the program
+        If this was not what was asked from the homework, and that it was expected that the user 
+        can run the help text _and_ use the progam, simply copy the code over to where helpflag = true above
+        */
+        
+    }
+    
+    //start of actual program
+    char temp_char{'y'};
     char input_char{'x'};
     std::string user_input = "";
     std::string output = "";
     
     while(std::cin >> input_char){
-        in_char = input_char;
-        if(isalpha(in_char)){
-            in_char = toupper(in_char);
-            output.push_back(in_char);
+        temp_char = input_char;
+        if(isalpha(temp_char)){
+            temp_char = toupper(temp_char);
+            output.push_back(temp_char);
         }
-        if(isdigit(in_char)){
-            output.append(changenumtochar(in_char));
+        if(isdigit(temp_char)){
+            output.append(changenumtochar(temp_char));
         }
     }
+    std::cout << output << std::endl;
 
-    /* Initial attempt
+    /* Initial attempt (saved for posterity)
     std::cin >> user_input; //asks for user input to transliterate (part of initial attempt)
     for(int i{0}; i < user_input.length(); ++i){
         in_char = user_input[i];
@@ -111,6 +125,6 @@ int main(int argc, char* argv[]){
     }
     */
 
-    std::cout << output << std::endl;
+    
 
 }
